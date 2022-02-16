dca5b2b595c834ab016cf2063e43c26d3c31531d
oliverweighell <oli.weighell@digital.justice.gov.uk>
2022-02-16 13:24:04 +0000

    Add suppport for drapzone file uploads

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,12 +7,24 @@ if (window.console && window.console.info) {
 
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
+
+  // postcode lookup
   $('#find-address, .postcode_change a').click(function() {
     $('.togglePostcode').toggleClass('govuk-visually-hidden');
     var postcode = $('input#postcode').val();
     $('label.postcode_change').text(postcode);
   });
 
+  // status tags
   $('.not_started, .blocked').find('.govuk-tag').addClass("govuk-tag--grey");
   $('.in_progress').find('.govuk-tag').addClass("govuk-tag--blue");
+
+  // file uploads
+  if(typeof MOJFrontend.MultiFileUpload !== 'undefined') {
+    new MOJFrontend.MultiFileUpload({
+      container: $('.moj-multi-file-upload'),
+      uploadUrl: '/ajax-upload-url',
+      deleteUrl: '/ajax-delete-url'
+    });
+  }
 })
