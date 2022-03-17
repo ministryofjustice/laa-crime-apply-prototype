cadc70f3aa628053b2cabee73c4f19a0f486a0ba
oliverweighell <oli.weighell@digital.justice.gov.uk>
2022-03-17 15:47:02 +0000

    Update routing of means section

---
+++ app/data/hmrc-record.js
@@ -0,0 +1,11 @@
+module.exports = {
+  employment_status: 'employed',
+  employed_income: {
+    paye: [
+      {
+        'paye_date': '2021-01-01',
+        'paye_amount': '1234'
+      }
+    ]
+  }
+};
